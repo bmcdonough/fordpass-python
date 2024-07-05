40aa4cd8abfdd48483259ee1bd50702547a6b2bb
Bill McDonough <wilmcd@gmail.com>
2024-07-05 22:23:30 +0000

    saving my work

--- bmcdonough.py
+++ bmcdonough.py
@@ -3,6 +3,11 @@ import logging
 import logging.handlers
 import os
 
+from fordpass import Vehicle
+from fordpass.constants import (
+    REGIONS,
+    REGION_OPTIONS
+)
 from dotenv import load_dotenv
 
 
@@ -14,6 +19,15 @@ def config_logging():
     )
     return logging.getLogger(__name__)
 
+def get_region_details(region):
+    if region in REGION_OPTIONS:
+        region_details = REGIONS.get(region)
+        _LOGGER.debug(f"get_region_details: {region_details}")
+        return(region_details)
+    else:
+        _LOGGER.error("region:{region} not in REGION_OPTIONS:{REGION_OPTIONS}")
+        return None
+
 
 def main():
     try:
@@ -21,12 +35,19 @@ def main():
             # Load environment variables from the .env file
             load_dotenv()
             fp_username = os.environ.get("FORDPASS_USERNAME")
+            fp_password = os.environ.get("FORDPASS_PASSWORD")
+            fp_vin = os.environ.get("FORDPASS_VIN")
             fp_region = os.environ.get("FORDPASS_REGION")
             if (fp_username or fp_region) is None:
                 _LOGGER.warning(
                     f"if either fp_username:[{fp_username}] or fp_region:[{fp_region}] is None, stop"
                 )
                 return 1
+            _LOGGER.debug(get_region_details(fp_region))
+            r = Vehicle(username=fp_username, password=fp_password, vin=fp_vin, region=fp_region)  # Username, Password, VIN
+            print(r.status())  # Print the status of the car
+
+
             return 0
         else:
             _LOGGER.warning("unable to load .env")
--- fordpass/fordpass.py
+++ fordpass/fordpass.py
@@ -1,6 +1,16 @@
+import hashlib
+import random
+import string
 import requests
 import logging
 import time
+
+from .constants import (
+    REGION_OPTIONS,
+    REGIONS,
+)
+from base64 import urlsafe_b64encode
+
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -19,18 +29,27 @@ apiHeaders = {
 
 baseUrl = 'https://usapi.cv.ford.com/api'
 
+session = requests.Session()
+
 class Vehicle(object):
     '''Represents a Ford vehicle, with methods for status and issuing commands'''
 
-    def __init__(self, username, password, vin):
+    def __init__(self, username, password, vin, region):
         self.username = username
         self.password = password
         self.vin = vin
+        self.region = region
         self.token = None
         self.expires = None
+        self.login_input = {}
     
     def auth(self):       
         '''Authenticate and store the token'''
+
+        self.region_details = self.__region_details()
+        _LOGGER.debug(f"region_details: {self.region_details}")
+
+        initial_url = self.generate_url() 
 
         data = {
             'client_id': '9fb503e0-715b-47e8-adfd-ad4b7770f73b',
@@ -44,9 +63,31 @@ class Vehicle(object):
             'Content-Type': 'application/x-www-form-urlencoded'
         }
 
-        r = requests.post('https://fcis.ice.ibmcloud.com/v1.0/endpoint/default/token', data=data, headers=headers)        
+#        r = requests.post('https://fcis.ice.ibmcloud.com/v1.0/endpoint/default/token', data=data, headers=headers)
+# GET
+# https://login.ford.com/4566605f-43a7-400a-946e-89cc9fdb0bd7/B2C_1A_SignInSignUp_en-US/oauth2/v2.0/authorize?redirect_uri=fordapp://userauthorized&response_type=code&max_age=3600&code_challenge=meXP5e9rTz50hB4Ws20WqOrzi_I2H3SpVwJiiqWavQo&code_challenge_method=S256&scope=%2009852200-05fd-41f6-8c21-d36d3497dc64%20openid&client_id=09852200-05fd-41f6-8c21-d36d3497dc64&ui_locales=en-US&language_code=en-US&country_code=USA&ford_application_id=71A3AD0A-CF46-4CCF-B473-FC7FE5BC4592
+        r = session.get(initial_url, headers=headers)
+        _LOGGER.debug(f"r.status_code: {r.status_code}")
+        # 'apparent_encoding', 'close', 'connection', 'content', 'cookies', 'elapsed', 'encoding', 'headers', 'history', 'is_permanent_redirect', 'is_redirect', 'iter_content', 'iter_lines', 'json', 'links', 'next', 'ok', 'raise_for_status', 'raw', 'reason', 'request', 'status_code', 'text', 'url']
+        _LOGGER.debug(f"dir(r):: {dir(r)}")
 
         if r.status_code == 200:
+# POST
+# https://login.ford.com/4566605f-43a7-400a-946e-89cc9fdb0bd7/B2C_1A_SignInSignUp_en-US/SelfAsserted?tx=StateProperties=eyJUSUQiOiIzMzlkN2E0YS02MmNlLTQwNGEtYTU2My01YjI5ODdkZDMzZGYifQ&p=B2C_1A_SignInSignUp_en-US
+# payload {
+#  Query String Parameters {
+#    tx: StateProperties=eyJUSUQiOiIzMzlkN2E0YS02MmNlLTQwNGEtYTU2My01YjI5ODdkZDMzZGYifQ
+#    p: B2C_1A_SignInSignUp_en-US
+#  }
+#  Form Data {
+#    request_type: RESPONSE
+#    signInName: USERNAME
+#    password: PASSWORD
+#  }
+#}
+# RESPONSE HEADERS
+# Location:
+
             _LOGGER.info('Succesfully fetched token')
             result = r.json()
             self.token = result['access_token']
@@ -54,7 +95,28 @@ class Vehicle(object):
             return True
         else:
             r.raise_for_status()
-    
+
+    def generate_url(self):
+        region = self.region
+        code1 = ''.join(random.choice(string.ascii_lowercase) for i in range(43))
+        code_verifier = self.generate_hash(code1)
+        self.login_input["code_verifier"] = code1
+        url = f"""{REGIONS[region]["locale_url"]}/4566605f-43a7-400a-946e-89cc9fdb0bd7/B2C_1A_SignInSignUp_{REGIONS[region]["locale"]}/oauth2/v2.0/authorize?redirect_uri=fordapp://userauthorized&response_type=code&max_age=3600&code_challenge={code_verifier}&code_challenge_method=S256&scope=%2009852200-05fd-41f6-8c21-d36d3497dc64%20openid&client_id=09852200-05fd-41f6-8c21-d36d3497dc64&ui_locales={REGIONS[region]["locale"]}&language_code={REGIONS[region]["locale"]}&country_code={REGIONS[region]["locale_short"]}&ford_application_id={REGIONS[region]["region"]}"""
+        _LOGGER.debug(f"generate_url:: {url}")
+        return url
+
+    def base64_url_encode(self, data):
+        """Encode string to base64"""
+        _LOGGER.debug(f"base64_url_encode:: {urlsafe_b64encode(data).rstrip(b'=')}")
+        return urlsafe_b64encode(data).rstrip(b'=')
+
+    def generate_hash(self, code):
+        """Generate hash for login"""
+        hashengine = hashlib.sha256()
+        hashengine.update(code.encode('utf-8'))
+        _LOGGER.debug(f"generate_hash:: {urlsafe_b64encode(hashengine.digest()).rstrip(b'=').decode('utf-8')}")
+        return self.base64_url_encode(hashengine.digest()).decode('utf-8')
+
     def __acquireToken(self):
         '''Fetch and refresh token as needed'''
 
@@ -151,3 +213,13 @@ class Vehicle(object):
             return self.__pollStatus(url, result['commandId'])
         else:
             command.raise_for_status()
+
+    def __region_details(self):
+        ''' get region details to build url '''
+        if self.region in REGION_OPTIONS:
+            region_details = REGIONS.get(self.region)
+            return(region_details)
+        else:
+            _LOGGER.error("region:{region} not in REGION_OPTIONS:{REGION_OPTIONS}")
+            return None
+
