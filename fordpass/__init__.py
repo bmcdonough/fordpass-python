6d613dd43640eb0c33156954d1cb4562713101dc
Bill McDonough <wilmcd@gmail.com>
2024-07-05 19:52:47 +0000

    modified constants

--- fordpass/__init__.py
+++ fordpass/__init__.py
@@ -1,16 +1,10 @@
-from .const import (
-    CONF_DISTANCE_UNIT,
-    CONF_PRESSURE_UNIT,
-    DEFAULT_DISTANCE_UNIT,
-    DEFAULT_PRESSURE_UNIT,
-    DOMAIN,
+from .constants import (
     MANUFACTURER,
     REGION,
+    REGION_OPTIONS,
+    REGIONS,
     VEHICLE,
     VIN,
-    UPDATE_INTERVAL,
-    UPDATE_INTERVAL_DEFAULT,
-    COORDINATOR
 )
 
 from .fordpass import Vehicle
